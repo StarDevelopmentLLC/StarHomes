e56fb3fa172838c80c720f43a5527e1ddb04356f
Firestar311 <slaterandrew0398@gmail.com>
2025-11-23 17:58:11 -0600

    Added warnings for the StarMCLib and StarCore plugins in the standalone build

--- standalone/src/main/java/com/stardevllc/starhomes/plugin/StandaloneStarHomesPlugin.java
+++ standalone/src/main/java/com/stardevllc/starhomes/plugin/StandaloneStarHomesPlugin.java
@@ -4,10 +4,28 @@ import com.stardevllc.starhomes.StarHomes;
 import com.stardevllc.starhomes.commands.*;
 import com.stardevllc.starmclib.StarMCLib;
 import com.stardevllc.starmclib.plugin.ExtendedJavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 public class StandaloneStarHomesPlugin extends ExtendedJavaPlugin {
     @Override
     public void onEnable() {
+        Plugin starmclibPlugin = Bukkit.getPluginManager().getPlugin("StarMCLib");
+        if (starmclibPlugin != null) {
+            getLogger().severe("StarMCLib plugin detected with the StarHomes-Standalone plugin");
+            getLogger().severe("Please either replace StarHomes-Standalone with StarHomes Plugin or remove StarMCLib plugin");
+            getLogger().severe("Please see the wiki page for more information");
+            getLogger().severe("https://github.com/StarDevelopmentLLC/StarHomes/wiki/Available-Binaries");
+        }
+        
+        Plugin starcorePlugin = Bukkit.getPluginManager().getPlugin("StarCore");
+        if (starcorePlugin != null) {
+            getLogger().severe("StarCore plugin detected with the StarHomes-Standalone plugin");
+            getLogger().severe("Please either replace StarHomes-Standalone with StarHomes Plugin or remove StarCore");
+            getLogger().severe("Please see the wiki page for more information");
+            getLogger().severe("https://github.com/StarDevelopmentLLC/StarHomes/wiki/Available-Binaries");
+        }
+        
         StarMCLib.init(this);
         super.onEnable();
         StarHomes.init(this);
